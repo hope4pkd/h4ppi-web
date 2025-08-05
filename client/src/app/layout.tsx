c2cd3582654247da5f4c64805a03d5b01aa649ad
Onyekachi David Nwakaihe <dnwakaihe@gmail.com>
2025-08-05 23:25:12 +0100

    :star: feat: add metadata and logo

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,10 +1,13 @@
 import { Provider } from "@/components/ui/provider";
 import type { Metadata } from "next";
 import "./globals.css";
+import logo from "@public/assets/logo.png";
+import favicon from "@public/assets/h4ppi_logo.ico";
 
 const appName = "Hope4PKD Patients Initiative";
 const appMetaTitle = `${appName} - Restoring Hope, one Kidney at a time.`;
-const appMetaDescription = "Nigeria's first platform that connects Polycystic Kidney Disease (PKD) patients with verified kidney donors and crowdfunded financial aid. #Hope4PKD #Hope4PKDPatients #Hope4PKDPatientsInitiative #Health #Healthcare #HealthTech #PKD";
+const appMetaDescription =
+  "Nigeria's first platform that connects Polycystic Kidney Disease (PKD) patients with verified kidney donors and crowdfunded financial aid. #Hope4PKD #Hope4PKDPatients #Hope4PKDPatientsInitiative #Health #Healthcare #HealthTech #PKD";
 const imageAlt = "Hope4PKD Patients Initiative Logo";
 
 export const metadata: Metadata = {
@@ -12,9 +15,9 @@ export const metadata: Metadata = {
   description: appMetaDescription,
   icons: {
     icon: [
-      { url: '/hope4pkd_logo.ico', type: 'image/x-icon' },     
+      { url: favicon.src, type: "image/x-icon" },
       {
-        url: "/hope4pkd_logo.png",
+        url: logo.src,
         type: "image/png",
         sizes: "any",
       },
@@ -28,13 +31,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html suppressHydrationWarning lang="en" >
-      <body
-        className={`antialiased`}
-      >
-        <Provider>
-          {children}
-        </Provider>
+    <html suppressHydrationWarning lang="en">
+      <body className={`antialiased`}>
+        <Provider>{children}</Provider>
       </body>
     </html>
   );
